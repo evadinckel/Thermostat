0d094080f3a6ff5e746957eb3c317c86c53a7573
Eva.Dinckel <evadinckel@gmail.com>
2017-11-22 14:25:03 +0000

    adds minimum temperature

--- spec/SpecThermostat.js
+++ spec/SpecThermostat.js
@@ -1,11 +1,18 @@
 describe('Thermostat', function(){
+var thermostat;
 
-  var thermostat;
+beforeEach(function(){
   thermostat = new Thermostat();
+});
 
-  describe('Knows it\'s initial temperature', function(){
-    it('Is defaulted to 20 degrees', function(){
+
+
+  describe('intiial properties', function(){
+    it('has a temperature defaulted to 20 degrees', function(){
       expect(thermostat.temperature).toEqual(20)
+    });
+    it('has a minimum temperature of 10 degrees', function(){
+      expect(thermostat.minTemperature).toEqual(10)
     });
   });
 
@@ -15,5 +22,22 @@ describe('Thermostat', function(){
       expect(thermostat.temperature).toEqual(25)
     });
   });
+
+  describe('it has a down function', function(){
+    it('can decrease temperature', function(){
+      thermostat.decrease(3)
+      expect(thermostat.temperature).toEqual(17)
+    });
+    it('prevents the temperature to go lower than 10 degrees', function(){
+      thermostat.decrease(11)
+      expect(thermostat.temperature).toEqual(20)
+    });
+    it('prints a warning message about minimum temperature', function(){
+      expect(thermostat.decrease(11)).toEqual('Selected temperature lower than 10 degrees')
+    });
+  });
+
+
+
 
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,7 +1,15 @@
 function Thermostat (){
   this.temperature = 20;
+  this.minTemperature = 10;
 }
 
 Thermostat.prototype.increase = function(number){
   this.temperature += number;
+};
+
+Thermostat.prototype.decrease = function(number){
+  if (this.temperature - number < 10) {
+    return 'Selected temperature lower than 10 degrees';
+  }
+  this.temperature -= number;
 };
