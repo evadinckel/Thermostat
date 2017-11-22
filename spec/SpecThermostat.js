5219688f8a5556597d215c954c02114bde0eb116
Ignacio Panebianco <ignaciopanebianco@gmail.com>
2017-11-22 16:26:36 +0000

    Adds saving power mode and energy usage info

--- spec/SpecThermostat.js
+++ spec/SpecThermostat.js
@@ -5,14 +5,17 @@ beforeEach(function(){
   thermostat = new Thermostat();
 });
 
-
-
   describe('intiial properties', function(){
     it('has a temperature defaulted to 20 degrees', function(){
       expect(thermostat.temperature).toEqual(20)
     });
+
     it('has a minimum temperature of 10 degrees', function(){
       expect(thermostat.minTemperature).toEqual(10)
+    });
+
+    it('has a power saving mode', function(){
+      expect(thermostat.isSavingPower).toEqual(true)
     });
   });
 
@@ -21,6 +24,27 @@ beforeEach(function(){
       thermostat.increase(5)
       expect(thermostat.temperature).toEqual(25)
     });
+
+    it('prevents the temperature to go higher than 25 degrees when saving power mode is on', function(){
+      thermostat.increase(6)
+      expect(thermostat.temperature).toEqual(20)
+    });
+
+    it('prints a warning about maximum temperature when saving power mode is on', function(){
+      expect(thermostat.increase(6)).toEqual('Selected temperature higher than 25 degrees')
+    });
+
+    it('prevents the temperature to go higher than 32 degrees when saving power mode is off', function(){
+      thermostat.isSavingPower = false
+      thermostat.increase(13)
+      expect(thermostat.temperature).toEqual(20)
+    });
+
+    it('prints a warning about maximum temperature when saving power mode is off', function(){
+      thermostat.isSavingPower = false
+      expect(thermostat.increase(13)).toEqual('Selected temperature higher than 32 degrees')
+    });
+
   });
 
   describe('it has a down function', function(){
@@ -37,7 +61,29 @@ beforeEach(function(){
     });
   });
 
+  describe('it has a reset option', function(){
+    it('resets temperature to 20', function(){
+      thermostat.increase(2)
+      thermostat.reset()
+      expect(thermostat.temperature).toEqual(20)
+    });
+  });
 
+  describe('it knows its energy usage level', function() {
+    it('returns "low-usage" when temperature is lower than 18 degrees', function(){
+      thermostat.decrease(3)
+      expect(thermostat.energyUsage()).toEqual('low-usage')
+    });
 
+    it('returns "high-usage" when temperature is above 25 degrees', function(){
+      thermostat.isSavingPower = false
+      thermostat.increase(10)
+      expect(thermostat.energyUsage()).toEqual('high-usage')
+    });
 
+    it('returns "medium-usage" when temperature is between 18 and 25 degrees', function(){
+      thermostat.increase(4)
+      expect(thermostat.energyUsage()).toEqual('medium-usage')
+    });
+  });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,9 +1,15 @@
 function Thermostat (){
   this.temperature = 20;
   this.minTemperature = 10;
+  this.isSavingPower = true;
 }
 
 Thermostat.prototype.increase = function(number){
+  if (this.isSavingPower && this.temperature + number > 25){
+    return 'Selected temperature higher than 25 degrees';
+  } else if(!this.isSavingPower && this.temperature + number > 32){
+    return 'Selected temperature higher than 32 degrees';
+  }
   this.temperature += number;
 };
 
@@ -12,4 +18,14 @@ Thermostat.prototype.decrease = function(number){
     return 'Selected temperature lower than 10 degrees';
   }
   this.temperature -= number;
+};
+
+Thermostat.prototype.reset = function (){
+  this.temperature = 20;
+};
+
+Thermostat.prototype.energyUsage = function(){
+  if(this.temperature < 18) return 'low-usage';
+  if(this.temperature > 25) return 'high-usage';
+  return 'medium-usage';
 };
